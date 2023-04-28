a77d432d0394b966661fd6818550d32545530f11
EduardoNogueira <eduardocastroemp@gmail.com>
2023-04-28 13:17:23 -0300

    integração com o servidor

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client"
 import { FastifyInstance } from "fastify"
+import { z } from 'zod'
 import consultHost  from "../scripts/consult-host.js"
 import dayjs from "dayjs"
 
@@ -7,17 +8,25 @@ const prisma = new PrismaClient()
 
 export async function appRoutes(app: FastifyInstance) {
 
-    app.post('/consulta', async (req, reply) => {
+    app.post('/consult', async (request) => {
+
+        const createIpBody = z.object({
+            ip: z.string()
+        })
+
+        const { ip } = createIpBody.parse(request.body)
+
 
         async function chamaFunção() {
-            const resultado = await consultHost('172.16.8.1')
+            const resultado = await consultHost(ip)
             return resultado
         }
     
         const consulta = await chamaFunção()
+
         const today = dayjs().startOf('day').toDate()
-    
-        /* for (let i = 0; i < consulta.length; i ++){
+        
+        for (let i = 0; i < consulta.length; i ++){
             await prisma.consult.create({
                 data: {
                     ipTarget: consulta[i].ip,
@@ -28,10 +37,21 @@ export async function appRoutes(app: FastifyInstance) {
                     created_at: today
                 }
             })
-        } */
+        }
+
         return consulta
     })
 
+    app.get('/consult', async (request) => {
+
+        await prisma.consult.findMany(
+            
+        )
+
+
+
+
+    })
 
 }
 
--- web/src/components/teste.tsx
+++ web/src/components/teste.tsx
@@ -1,12 +1,40 @@
+import { FormEvent, useEffect, useState } from 'react'
 import style from '../styles/global.css'
+import { api } from '../lib/axios'
 
 export default function Teste() {
+    const [ ip, setIp ] = useState('')
+    const [ resposta, setResposta ] = useState([])
+    
+    async function createConsult(event: FormEvent) {
+        event.preventDefault()
+
+        if (ip.length == 0){
+            return
+        }
+
+        await api.post('consult', {
+            ip
+        }).then(response => (
+            console.log(response.data)
+        )).then(response => (
+            setResposta(response.data)
+        ))
+
+    }
+    
+    
     return (
         <main className='h-80 w-80 text-2xl m-auto bg-red-500 flex flex-col justify-around items-center'>
             <h1 className='text-center font-bold f'>Consultar Host</h1>
-            <form action="./consulta"  className=' h-24 flex flex-col justify-around items-center'>
+            <form onSubmit={createConsult} className=' h-24 flex flex-col justify-around items-center'>
                 <label className='text-center'> Digite o Ip para consultar as informações do Host</label>
-                <input type="text" placeholder='Digite o ip'/>
+                <input 
+                    name='ip'
+                    type="text" 
+                    placeholder='Digite o ip'
+                    onChange={event => setIp(event.target.value)}    
+                />
                 <input type='submit' className='bg-blue-700 w-48 '/>
             </form>
         </main>
