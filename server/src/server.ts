4e37e20f4edd0062b74fca016be87424cf6d01b8
Eduardo Nogueira de Castro <eduardocastroemp@gmail.com>
2023-09-12 16:21:57 -0300

    alterando o axios para 127

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,6 @@ import Fastify from 'fastify'
 import cors from '@fastify/cors'
 import { appRoutes } from './routes'
 import dotenv from 'dotenv';
-
 dotenv.config()
 
 const app = Fastify()
@@ -10,14 +9,11 @@ const app = Fastify()
 app.register(cors)
 app.register(appRoutes)
 
-const porta = process.env.BACKEND_PORT;
-
-
 app.listen({
     port: 3333, 
     host: '0.0.0.0'
 }).then((address) => {
-    console.log(`Server running on ${address} \n`, porta)
+    console.log(`Server running on ${address} \n`,)
 }).catch( err => {
     console.log('Erro starting server: ', err, )
     process.exit(1)
