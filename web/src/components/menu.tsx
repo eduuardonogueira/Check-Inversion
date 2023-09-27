7e5dbedd594fc20012e3bac669861b541554633b
Eduardo Nogueira de Castro <eduardocastroemp@gmail.com>
2023-09-27 17:57:53 -0300

    created menu search

--- web/src/components/menu.tsx
+++ web/src/components/menu.tsx
@@ -2,11 +2,25 @@ import logo from '/src/img/logo-poppa-new.png'
 import { Link } from 'react-router-dom'
 import { Input } from './'
 import { Broadcast, HardDrives, House, PlusSquare } from '@phosphor-icons/react'
+import { api } from '../lib/axios'
+import { useState, useEffect } from 'react'
+import { HostsDB } from '../types/queryTypes'
 
 const activeColor = {color: "#548FE7"}
 
-export const Menu = ({ active }:{active: string}) => (
-    <header>
+export function Menu ({ active }:{active: string}) {
+    const [ search, setSearch ] = useState('')
+    const [ hosts, setHosts ] = useState<Array<HostsDB>>([])
+
+    const hostsFilter = hosts.filter( host => search !== '' && host.hostname.toLowerCase().includes(search.toLowerCase()))
+
+    useEffect(() => {
+        api.get('/hosts').then((response) => (setHosts(response.data), console.log(response.data)))
+    }, [])
+
+ 
+    return (
+        <header>
             <nav className='flex h-[100px] z-10 bg-[#202024] items-center justify-between px-8 box-border'>
                 <h1 className="w-[120px] h"><img src={logo} alt="logo" /></h1>
             
@@ -37,11 +51,28 @@ export const Menu = ({ active }:{active: string}) => (
                     </li>
                 </ul>
 
-                <Input 
-                    style={{width: "20"}}
-                    type='text'
-                    placeholder='Pesquisar'
-                />
+                <div className='flex w-[20%] flex-col relative'>
+                    <Input 
+                        type='text'
+                        placeholder='Pesquisar'
+                        onchange={(event) => (setSearch(event.target.value))}
+                    />
+                    
+                    {/*TODO: when you change the input focus, hide the search */}
+
+                    <ul className='absolute top-14 flex w-full flex-col '>
+                        {hostsFilter.map((item, index) => (
+                            <li 
+                                className='rounded-sm bg-[#fff] p-2' 
+                                key={index}
+                            > 
+                                <p>{item.hostname} | {item.ip}</p>
+                                <p>Neighbors: {item.neighbors.length}</p> 
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </nav>
         </header>
-) 
+    )
+} 
