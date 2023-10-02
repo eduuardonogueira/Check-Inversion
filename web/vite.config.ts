ece8fd742f583e8c622817480f8b4aafba175376
Eduardo Nogueira de Castro <eduardocastroemp@gmail.com>
2023-10-02 17:59:11 -0300

    actived enviroments from frontend

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,17 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { config } from 'dotenv'
+config()
 
-const portString = '5002'
-const port = parseInt(portString)
-
-
-// https://vitejs.dev/config/
 export default defineConfig({
-
   plugins: [react()],
   server: {
-    port: port,
-    host: "0.0.0.0" 
+    port: Number(process.env.VITE_FRONTEND_PORT) || 5002, 
+    host: process.env.VITE_FRONTEND_HOST || "0.0.0.0"
   }
 })
 
